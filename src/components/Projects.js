a2cfcc7674a623f5a4ddb28738bb9d623765af3e
Brian Kilpatrick <thebriankilpatrick@gmail.com>
2020-07-31 16:48:12 -0400

    Added projects file in components

---
+++ src/components/Projects.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Projects() {
+    return (
+        <>
+            <h1>Project template goes here.</h1>
+        </>
+    )
+}
+
+export default Projects;
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,4 +1,5 @@
 import React from 'react';
+// import Projects from "../components/Projects";
 
 function Main() {
     return (
