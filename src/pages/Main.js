fc6aeb819cb22b061b94fc8997ad867590b0bd74
Brian Kilpatrick <thebriankilpatrick@gmail.com>
2020-08-05 23:06:50 -0400

    lightly pseudocoded my plan for the site

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,6 +1,15 @@
 import React from 'react';
 // import Projects from "../components/Projects";
 
+// Want to have a screen-sized 'landing' image, that will fit any size of screen
+// This 'image' will have some sort of cool CSS effect, and will have my name, and what I do in the center
+// The user could either click 'Learn More', or just scroll.
+// Clicking 'Learn More' will have a JS scroll effect to bring the user to the about me section
+// Unless I can get a good picture of myself, I probably will not have a picture on the site.
+// I will probably have those 'icon' things with some of the regular tech terminology
+// Will need to find a unique way to list the projects I have done.
+// Then, further down, the contact portion.  And possibly, a button to go back to the top.
+
 function Main() {
     return (
         <>
